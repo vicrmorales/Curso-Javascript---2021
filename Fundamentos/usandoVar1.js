fd91074d4cd18156a9d3d453b073d7045de61002
Victor <victorcruzmorales@gmail.com>
2021-11-13 00:12:32 -0300

    Quinta aula

---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,22 @@
+{
+    {
+        {
+            {
+                {
+                    var sera = 'Será???'
+                    console.log(sera)
+                }
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local) //variavel 'local' não fica disponivel fora da função
