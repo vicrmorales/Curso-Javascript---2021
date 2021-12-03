698af468eb7673dc16473103f4fb09a1c6c6f2e2
Victor <victorcruzmorales@gmail.com>
2021-12-03 01:33:10 -0300

    18ª aula

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+// CODIGO NÃO EXECUTAVEL
+
+//Procedural
+processamento(valor1,valor2,valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // Foca passou a ser o objeto
+
+// Principios importantes:
+// 1.abstração - Só pegar os componentes importantes
+// 2.emcapsulamento - Só deixar expostos os itens necessários para o usuario(pedal,direção,marchar)
+// 3.herança (prototype) - Conceito de "É um"
+// 4.polimorfismo - Usar uma variavel genérica para um objeto no tipo Carro-> Uno
